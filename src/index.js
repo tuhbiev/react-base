a4e69af838711b3490a00d43533b37404604d0ae
tuhbiev <51776392+tuhbiev@users.noreply.github.com>
2021-04-17 20:45:54 +0300

    propTypes Подключение propTypes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 const render = ReactDOM.render;
 
 const node = document.getElementById("root");
-const root = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "h1",
-    {},
-    "Hello World!",
-    React.createElement(
-      "a",
-      { href: "https://www.google.ru/" },
-      React.createElement("h1", {}, "React In Action"),
-      React.createElement("em", {}, "...and now it really is!")
-    )
-  )
-);
-render(root, node);
+class Post extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "post"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "postAuthor",
+          id: this.props.id
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "postBody"
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Post.propTypes = {
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+const App = React.createElement(Post, {
+  id: 1,
+  content: " said: This is post!",
+  user: "mark"
+});
+
+render(App, node);
