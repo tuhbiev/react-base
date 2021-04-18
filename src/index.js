d2f82b22427f48d5149c203a84f934b0ad6e068d
tuhbiev <51776392+tuhbiev@users.noreply.github.com>
2021-04-18 21:37:42 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import PropTypes from "prop-types";
-const render = ReactDOM.render;
 
 const node = document.getElementById("root");
+
 class Post extends Component {
   render() {
     return React.createElement(
@@ -24,7 +24,8 @@ class Post extends Component {
             className: "postBody"
           },
           this.props.content
-        )
+        ),
+        this.props.children
       )
     );
   }
@@ -36,10 +37,84 @@ Post.propTypes = {
   id: PropTypes.number.isRequired
 };
 
-const App = React.createElement(Post, {
-  id: 1,
-  content: " said: This is post!",
-  user: "mark"
-});
+class Comment extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "comment"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "commentAuthor"
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "commentContent"
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
+};
+
+class CreateComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+      user: ""
+    };
+  }
+  render() {
+    return React.createElement(
+      "form",
+      {
+        className: "createComment"
+      },
+      React.createElement("input", {
+        type: "text",
+        placeholder: "Your name",
+        value: this.state.user
+      }),
+      React.createElement("input", {
+        type: "text",
+        placeholder: "Thoughts?"
+      }),
+      React.createElement("input", {
+        type: "submit",
+        value: "Post"
+      })
+    );
+  }
+}
+CreateComment.propTypes = {
+  content: PropTypes.string
+};
+
+const App = React.createElement(
+  Post,
+  {
+    id: 1,
+    content: " said: This is a post!",
+    user: "mark"
+  },
+  React.createElement(Comment, {
+    id: 2,
+    user: "bob",
+    content: " commented: wow! how cool!"
+  }),
+  React.createElement(CreateComment)
+);
 
 render(App, node);
